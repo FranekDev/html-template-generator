a61e9fa96f4cba99dd3732c8ed16820643c03c4a
Franek Wojtas <49079734+FranekDev@users.noreply.github.com>
2024-05-04 14:18:36 +0200

    refactor: update Specification model

--- src/HtmlTemplateGenerator/Models/Specification.cs
+++ src/HtmlTemplateGenerator/Models/Specification.cs
@@ -4,37 +4,7 @@ public record Specification
 {
     public string? Title { get; init; } = null;
     public string? Text { get; set; } = null;
-    public IEnumerable<SpecificationItem> Items { get; set; } = [];
-
-    public IEnumerable<SpecificationItem> GenerateSpecificationItems(string text)
-    {
-        if (Text is null)
-        {
-            return [];
-        }
-        
-        var items = text.Split("\n");
-        var specificationItems = new List<SpecificationItem>();
-        
-        foreach (var item in items)
-        {
-            var keyValuePair = item.Split(":");
-            if (keyValuePair.Length != 2)
-            {
-                continue;
-            }
-            
-            var (key, value) = (keyValuePair[0].Trim(), keyValuePair[1].Trim());
-            if (string.IsNullOrWhiteSpace(key) || string.IsNullOrWhiteSpace(value))
-            {
-                throw new Exception("Specification item is not in the correct format. Please make sure that the key and value are separated by a colon (:) and that there are no leading or trailing spaces.");
-            }
-
-            specificationItems.Add(new SpecificationItem { Key = key, Value = value });
-        }
-
-        return specificationItems;
-    }
+    public List<SpecificationItem> Items { get; set; } = [];
 }
 
 public record SpecificationItem
